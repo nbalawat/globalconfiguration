8be955bed3b98b4a05a87c09930f227f356ef546
Naveen Balawat <naveen.balawat@otaras.com>
2022-01-04 01:51:12 -0500

    Added override capabilities to avoid formatting issues

--- nvim_alt1/.config/nvim/lua/lsp/language_servers.lua
+++ nvim_alt1/.config/nvim/lua/lsp/language_servers.lua
@@ -17,5 +17,14 @@ lsp_installer.on_server_ready(function(server)
 
     }, opts)
   end
+
+  if server.name == "tsserver" then
+    opts = vim.tbl_deep_extend("force", {
+        on_attach = function(client)
+          client.resolved_capabilities.document_formatting = false
+          client.resolved_capabilities.document_range_formatting = false
+        end,
+    }, opts)
+  end
   server:setup(opts)
 end)
